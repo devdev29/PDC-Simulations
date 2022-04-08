d9c96e8ef1042e559ddd9783d9afdb2d1792016b
Anurag Pandey <anurag.pandey3546@gmail.com>
2022-04-08 11:04:52 +0530

    lab 6 added

---
+++ PDCExperiment6.py
@@ -0,0 +1,72 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+bits=[]
+print("enter the bits")
+for i in range(8):
+    bits.append(int(input()))
+
+print("enter the frequency")
+fc=int(input())
+t=np.arange(0,8,0.01)
+c_00=np.sin(2*np.pi*fc*t)
+c_01=np.cos(2*np.pi*fc*t)
+c_10=-np.sin(2*np.pi*fc*t)
+c_11=-np.cos(2*np.pi*fc*t)
+
+#creating binary stream:
+interpolated_bits=[]
+
+for i in range(8):
+    for j in range(100):
+        interpolated_bits.append(bits[i])
+
+#generating qpsk wave
+
+qpsk=[]
+
+for i in np.arange(0,8,2):
+    for j in range(200):
+        if(interpolated_bits[i*100]==0):
+            if(interpolated_bits[i*100+100]==0):
+                qpsk.append(c_00[100*i+j])
+            else:
+                qpsk.append(c_01[100*i+j])
+
+        else:
+            if(interpolated_bits[i+100]==0):
+                qpsk.append(c_10[100*i+j])
+            else:
+                qpsk.append(c_11[100*i+j])
+
+        
+fig,axs=plt.subplots(6)
+axs[0].plot(t,interpolated_bits)
+axs[0].set_xlabel("time")
+axs[0].set_ylabel("bits")
+
+axs[1].plot(t,c_00)
+axs[1].set_xlabel("time")
+axs[1].set_ylabel("c_00")
+
+axs[2].plot(t,c_01)
+axs[2].set_xlabel("time")
+axs[2].set_ylabel("c_01")
+
+axs[3].plot(t,c_10)
+axs[3].set_xlabel("time")
+axs[3].set_ylabel("c_10")
+
+axs[4].plot(t,c_11)
+axs[4].set_xlabel("time")
+axs[4].set_ylabel("c_11")
+
+axs[5].plot(t,qpsk)
+axs[5].set_xlabel("time")
+axs[5].set_ylabel("c_00")
+
+plt.show()
+
+
+
